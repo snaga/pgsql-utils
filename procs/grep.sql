830b0ed1423a32832d812c7a5515f35c1f0f544c
Satoshi Nagayasu <snaga@uptime.jp>
2013-10-13 01:04:18 +0900

    Add support for multiple table search.

--- procs/grep.sql
+++ procs/grep.sql
@@ -9,31 +9,48 @@ AS $$
 DECLARE
   _searchkey ALIAS FOR $1;
   _relname ALIAS FOR $2;
+  _query1 TEXT;
   _query TEXT;
   _attlist TEXT;
-  _rec RECORD;
+  _rec1 RECORD;
+  _rec2 RECORD;
+  _rec3 RECORD;
 BEGIN
-  _attlist = '';
-  FOR _rec IN SELECT attname::text
-                FROM pg_attribute a, pg_class c
-               WHERE c.relname = _relname
-                 AND c.relkind IN ( 'r', 'v' )
-                 AND c.oid = a.attrelid 
-                 AND a.attnum > 0
-               ORDER BY a.attnum LOOP
-    IF _attlist = '' THEN
-      _attlist = 'coalesce("' || _rec.attname || '"::text,'''')';
-    ELSE
-      _attlist = _attlist || ' || ' || ''',''' || ' || coalesce("' || _rec.attname || '"::text,'''')';
-    END IF;
-  END LOOP;
+  _query1 = 'SELECT oid,relname
+               FROM pg_class
+              WHERE relname LIKE ''%' || _relname || '%''
+                AND relkind IN ( ''r'', ''v'' )
+              ORDER BY relname';
 
-  _query = 'SELECT ' || _attlist || ' AS line FROM "' || _relname || '" WHERE ' || _attlist || ' LIKE ''%%' || _searchkey || '%%''';
+  FOR _rec1 IN EXECUTE _query1 LOOP 
 
---   RAISE NOTICE '%', _query;
+--    RAISE NOTICE 'relname=%, oid=%', _rec1.relname, _rec1.oid;
 
-  FOR _rec IN EXECUTE _query LOOP
-    RETURN NEXT _rec.line;
+    --
+    -- build attribute list
+    --
+    _attlist = '';
+
+    FOR _rec2 IN SELECT attname::text
+                   FROM pg_attribute a
+                  WHERE a.attrelid = _rec1.oid
+                    AND a.attnum > 0
+                  ORDER BY a.attnum LOOP
+
+      IF _attlist = '' THEN
+        _attlist = 'coalesce("' || _rec2.attname || '"::text,'''')';
+      ELSE
+        _attlist = _attlist || ' || ' || ''',''' || ' || coalesce("' || _rec2.attname || '"::text,'''')';
+      END IF;
+    END LOOP;
+
+    _query = 'SELECT ' || _attlist || ' line FROM "' || _rec1.relname || '" WHERE ' || _attlist || ' LIKE ''%%' || _searchkey || '%%''';
+
+--    RAISE NOTICE '%', _query;
+
+    FOR _rec3 IN EXECUTE _query LOOP
+      RETURN NEXT _rec1.relname || ',' || _rec3.line;
+    END LOOP;
   END LOOP;
 END
 $$ LANGUAGE 'plpgsql';
